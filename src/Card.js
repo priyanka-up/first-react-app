65f1b929f4f56a097cc0bd74dcecc091c8e70041
priyanka upadhyaya <nehu10upadhyay@gmail.com>
2023-02-23 22:26:46 -0800

    Creating my first commit for react app

---
+++ src/Card.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Card extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <section className ="card-container">
+                    <header className ="card-header">
+                    <span initials = "PU" className= "card-header span"></span>
+                    <h2 className="card-header h2">Priyanka Upadhyaya</h2>
+                    </header>
+                    <main>
+                        <ul>
+                            <li><span>Birthday</span>Apr 10, 1989</li>
+                            <li><span>Address</span>11356 26th ave</li>
+                            <li><span>Phone</span>7818279344</li>
+                            
+
+                        </ul>
+                    </main>
+                </section>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Card;
